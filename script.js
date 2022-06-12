ff7e8df07adb88df429b0e9e0793c13adcac3ca7
Victor <86137531+eovictoor@users.noreply.github.com>
2022-06-12 14:54:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -23,3 +23,32 @@ function checkSenha(){
         Senha.type = "password"
     }
 }
+
+const formLogin = document.querySelector(".login-form")
+const formRegister = document.querySelector(".register-form")
+const loginButton = document.querySelector(".content-01")
+const registerButton = document.querySelector(".content-02")
+
+const loginUnder = document.querySelector(".h11")
+const registerUnder = document.querySelector(".h12")
+
+
+
+loginButton.addEventListener("click", ()=>{
+    formLogin.style.transform = "rotateY(0)"
+    formRegister.style.transform = "rotateY(90deg)"
+    loginUnder.style.textDecoration = "underline"
+    loginUnder.style.color = "red"
+    registerUnder.style.color = "white"
+    registerUnder.style.textDecoration = "none"
+})
+
+registerButton.addEventListener("click", ()=>{
+    formLogin.style.transform = "rotateY(90deg)"
+    formRegister.style.transform = "rotateY(0)"
+    loginUnder.style.textDecoration = "none"
+    loginUnder.style.color = "white"
+    registerUnder.style.color = "red"
+    registerUnder.style.textDecoration = "underline"
+})
+
